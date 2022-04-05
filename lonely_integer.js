7aeb9ab3bddf756504fad659932b6b68302280b9
Slicktone <tclaytony23@gmail.com>
2022-04-05 16:23:25 -0700

    xor didn't pass

---
+++ lonely_integer.js
@@ -0,0 +1,11 @@
+// trying to solve this using XOR operator (bitwise operations)
+/* function lonelyinteger(a) {
+    const xorReduce = (array) => {
+        let a = array.reduce((a,b) => a ^ b,0);
+
+
+    }
+    return a
+}
+*/
+
